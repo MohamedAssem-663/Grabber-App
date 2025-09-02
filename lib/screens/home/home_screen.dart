5232c689536a6082507bd4f176b4b46975254db7
Mohamed Assem <mohamedassem663@gmail.com>
2025-09-02 14:56:20 +0300

    add assets

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -5,6 +5,21 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            Image.asset('assets/Icons/bike.png', height: 35),
+            SizedBox(width: 10),
+            Text('61 Hopper street..'),
+            SizedBox(width: 10),
+            Icon(Icons.keyboard_arrow_down_outlined),
+            Spacer(),
+            Image.asset('assets/Icons/Vectorr.png', height: 30),
+          ],
+        ),
+      ),
+    );
   }
 }
